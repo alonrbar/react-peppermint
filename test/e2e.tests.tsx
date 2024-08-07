ebe323062773e04302e854f675233adb3a4fc47a
alonrbar <alon.r.bar@gmail.com>
2024-08-07 13:21:23 +0300

    Functional component withViewModel test

--- test/e2e.tests.tsx
+++ test/e2e.tests.tsx
@@ -12,6 +12,10 @@ describe('End to end', () => {
         public increment() {
             this.count++;
         }
+
+        public reset() {
+            this.count = 0;
+        }
     }
 
     const vm = new CounterViewModel();
@@ -21,6 +25,10 @@ describe('End to end', () => {
             return vm as any;
         }
     };
+
+    beforeEach(() => {
+        vm.reset();
+    });
 
     test('Class component withViewModel', () => {
 
@@ -49,5 +57,32 @@ describe('End to end', () => {
 
         // Verify UI updated
         ui.getByText('Count: 1');
+    });
+
+    test('Functional component withViewModel', () => {
+
+            // Define a functional component
+            const TestComponent = (props: { count: number }) => {
+                return <div>Count: {props.count}</div>
+            }
+
+            // Bind the view model to the component
+            const BoundTestComponent = withViewModel(CounterViewModel)(TestComponent);
+
+            // Render
+            const ui = render(
+                <Provider resolver={simpleResolver}>
+                    <BoundTestComponent />
+                </Provider>
+            )
+            ui.getByText('Count: 0');
+
+            // Update state
+            act(() => {
+                vm.increment();
+            });
+
+            // Verify UI updated
+            ui.getByText('Count: 1');
     });
 });
